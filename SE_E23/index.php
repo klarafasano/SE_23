0560b97d2b66400ac0b87268f356e013db50ca53
klarafasano <154306782+klarafasano@users.noreply.github.com>
2023-12-19 16:18:38 +0900

    Delete SE_E23 directory

--- SE_E23/index.php
+++
@@ -1,7 +0,0 @@
-<!-- Main Page -->
-<?php
-//include 'nuke_db.php';
-include_once 'functions/db.php';
-include 'pages/header.html';
-include ('pages/recipeCard.php');
-include ('functions/search_recipes.php');
